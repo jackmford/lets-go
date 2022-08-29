79899d7dff4a3751adf076dd4145ded3f6818cec
jackmford <jackmitchellfordyce@gmail.com>
2022-08-29 15:28:52 -0500

    chapter 2.4

--- snippetbox/main.go
+++ snippetbox/main.go
@@ -18,6 +18,12 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
+    if r.Method != "POST" {
+        w.Header().Set("Allow", http.MethodPost)
+        http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+        return
+    }
+
     w.Write([]byte("Create a new snippet..."))
 }
 
