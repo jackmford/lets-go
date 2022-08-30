4b50ee6d6d979c53083d7d9e22530db365ff4107
jackmford <jackmitchellfordyce@gmail.com>
2022-08-30 15:56:11 -0500

    chapter 2.7 - embedding partials

--- snippetbox/cmd/web/handlers.go
+++ snippetbox/cmd/web/handlers.go
@@ -2,6 +2,8 @@ package main
 
 import ( 
     "fmt"
+    "html/template"
+    "log"
     "net/http"
     "strconv"
 )
@@ -11,7 +13,23 @@ func home(w http.ResponseWriter, r *http.Request) {
         http.NotFound(w, r)
         return
     }
-    w.Write([]byte("Hello from Snippetbox")) 
+
+    files := []string{
+        "./ui/html/base.tmpl",
+        "./ui/html/pages/home.tmpl",
+    }
+    ts, err := template.ParseFiles(files...)
+    if err != nil {
+        log.Print(err.Error())
+        http.Error(w, "Internal Server Error", 500)
+        return
+    }
+
+    err = ts.ExecuteTemplate(w, "base", nil)
+    if err != nil {
+        log.Print(err.Error())
+        http.Error(w, "Internal Server Error", 500)
+    }
 }
 
 func snippetView(w http.ResponseWriter, r *http.Request) { 
