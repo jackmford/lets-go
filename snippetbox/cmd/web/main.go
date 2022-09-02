3c7373140f5c5d6c95932ac38f207e962706dfd0
jackmford <jackmitchellfordyce@gmail.com>
2022-09-02 12:02:32 -0500

    chatpers 2.9-3.1

--- snippetbox/cmd/web/main.go
+++ snippetbox/cmd/web/main.go
@@ -1,19 +1,25 @@
 package main
 
 import ( 
+    "flag"
     "log"
     "net/http"
 )
 
 func main() {
+    addr := flag.String("addr", ":4000", "HTTP network address")
+    flag.Parse()
+
     mux := http.NewServeMux()
+
     fileServer := http.FileServer(http.Dir("./ui/static/"))
     mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
     mux.HandleFunc("/", home) 
     mux.HandleFunc("/snippet/view", snippetView) 
     mux.HandleFunc("/snippet/create", snippetCreate)
 
-    log.Print("Starting server on :4000") 
-    err := http.ListenAndServe(":4000", mux) 
+    log.Printf("Starting server on %s", *addr) 
+    err := http.ListenAndServe(*addr, mux) 
     log.Fatal(err)
 }
