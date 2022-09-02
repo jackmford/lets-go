de591f0acd7508dc3ed5f0185ba3cba8c960446b
jackmford <jackmitchellfordyce@gmail.com>
2022-09-02 12:17:41 -0500

    chapter 3.2

--- snippetbox/cmd/web/main.go
+++ snippetbox/cmd/web/main.go
@@ -4,11 +4,15 @@ import (
     "flag"
     "log"
     "net/http"
+    "os"
 )
 
 func main() {
     addr := flag.String("addr", ":4000", "HTTP network address")
     flag.Parse()
+
+    infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+    errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
     mux := http.NewServeMux()
 
@@ -19,7 +23,13 @@ func main() {
     mux.HandleFunc("/snippet/view", snippetView) 
     mux.HandleFunc("/snippet/create", snippetCreate)
 
-    log.Printf("Starting server on %s", *addr) 
-    err := http.ListenAndServe(*addr, mux) 
-    log.Fatal(err)
+    srv := &http.Server{
+        Addr: *addr,
+        ErrorLog: errorLog,
+        Handler: mux,
+    }
+
+    infoLog.Printf("Starting server on %s", *addr) 
+    err := srv.ListenAndServe()
+    errorLog.Fatal(err)
 }
