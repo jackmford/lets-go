b299192ce515b3e5e4b1c1e6fe4e916319df7d91
jackmford <jackmitchellfordyce@gmail.com>
2022-09-21 14:47:24 -0500

    chapters 4.5 and 4.6

--- snippetbox/cmd/web/main.go
+++ snippetbox/cmd/web/main.go
@@ -7,12 +7,15 @@ import (
     "net/http"
     "os"
 
+    "snippetbox.jackmitchellfordyce.com/internal/models"
+
     _ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
     errorLog *log.Logger
     infoLog  *log.Logger
+    snippets *models.SnippetModel
 }
 
 func main() {
@@ -33,6 +36,7 @@ func main() {
     app := &application{
         errorLog: errorLog,
         infoLog: infoLog,
+        snippets: &models.SnippetModel{DB: db},
     }
 
     srv := &http.Server{
