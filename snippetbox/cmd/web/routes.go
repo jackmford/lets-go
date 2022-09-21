c38959f76b3404d00b46856b4707d45d6c755832
jackmford <jackmitchellfordyce@gmail.com>
2022-09-21 14:26:36 -0500

    chapters 4.3 and 4.4

--- snippetbox/cmd/web/routes.go
+++ snippetbox/cmd/web/routes.go
@@ -6,7 +6,7 @@ func (app *application) routes() *http.ServeMux {
     mux := http.NewServeMux()
 
     fileServer := http.FileServer(http.Dir("./ui/static/"))
-    mux.Handle("/static/", http.StripPrefix("/static", filerserver))
+    mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
     mux.HandleFunc("/", app.home)
     mux.HandleFunc("/snippet/view", app.snippetView)
