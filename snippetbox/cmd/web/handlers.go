0564f7fdcb36601937f8ad526ed700028f144aa8
jackmford <jackmitchellfordyce@gmail.com>
2022-10-11 14:56:43 -0500

    chapter 4.7

--- snippetbox/cmd/web/handlers.go
+++ snippetbox/cmd/web/handlers.go
@@ -1,10 +1,13 @@
 package main
 
 import ( 
+    "errors"
     "fmt"
     "html/template"
     "net/http"
     "strconv"
+
+    "snippetbox.jackmitchellfordyce.com/internal/models"
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) { 
@@ -37,7 +40,18 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
         return
     }
 
-    fmt.Fprintf(w, "Display a specific snippet with ID %d...", id) 
+    snippet, err := app.snippets.Get(id)
+    if err != nil {
+        if errors.Is(err, models.ErrNoRecord) {
+            app.notFound(w)
+        } else {
+            app.serverError(w, err)
+        }
+        return
+    }
+
+
+    fmt.Fprintf(w, "%+v", snippet)
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) { 
