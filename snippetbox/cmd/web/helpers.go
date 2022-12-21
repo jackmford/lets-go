386e4c0b039979784732eb54b08c45c8d4ba1073
jackmford <jackmitchellfordyce@gmail.com>
2022-12-21 15:37:01 -0600

    chapter 5.4 writing templates to buffer to catch runtime errors

--- snippetbox/cmd/web/helpers.go
+++ snippetbox/cmd/web/helpers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"net/http"
 	"runtime/debug"
@@ -14,12 +15,17 @@ func (app *application) render(w http.ResponseWriter, status int, page string, d
 		return
 	}
 
-	w.WriteHeader(status)
+	buf := new(bytes.Buffer)
 
-	err := ts.ExecuteTemplate(w, "base", data)
+	err := ts.ExecuteTemplate(buf, "base", data)
 	if err != nil {
 		app.serverError(w, err)
+		return
 	}
+
+	w.WriteHeader(status)
+
+	buf.WriteTo(w)
 }
 
 func (app *application) serverError(w http.ResponseWriter, err error) {
