2c17c0078f2aadca3cd639214e177b2a2aa188e5
jackmford <jackmitchellfordyce@gmail.com>
2022-12-23 11:32:54 -0600

    chapter 6.3 request logging middleware

--- snippetbox/cmd/web/middleware.go
+++ snippetbox/cmd/web/middleware.go
@@ -4,6 +4,14 @@ import (
 	"net/http"
 )
 
+func (app *application) logRequest(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		app.infoLog.Printf("%s - %s %s %s", r.RemoteAddr, r.Proto, r.Method, r.URL.RequestURI())
+
+		next.ServeHTTP(w, r)
+	})
+}
+
 func secureHeaders(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Security-Policy", "default-src 'self'; style-src 'self' fonts.google.com; font-src fonts.gstatic.com")
--- snippetbox/cmd/web/routes.go
+++ snippetbox/cmd/web/routes.go
@@ -12,5 +12,5 @@ func (app *application) routes() http.Handler {
 	mux.HandleFunc("/snippet/view", app.snippetView)
 	mux.HandleFunc("/snippet/create", app.snippetCreate)
 
-	return secureHeaders(mux)
+	return app.logRequest(secureHeaders(mux))
 }
