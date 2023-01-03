35bf38e2e26e982a3875b98fb051e0eaa9cd5375
jackmford <jackmitchellfordyce@gmail.com>
2023-01-03 15:10:42 -0600

    chapter 8.6 automatic form parsing

--- snippetbox/cmd/web/helpers.go
+++ snippetbox/cmd/web/helpers.go
@@ -2,11 +2,34 @@ package main
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
 	"net/http"
 	"runtime/debug"
 	"time"
+
+	"github.com/go-playground/form/v4"
 )
+
+func (app *application) decodePostForm(r *http.Request, dst any) error {
+	err := r.ParseForm()
+	if err != nil {
+		return err
+	}
+
+	err = app.formDecoder.Decode(dst, r.PostForm)
+	if err != nil {
+		var invalidDecoderError *form.InvalidDecoderError
+
+		if errors.As(err, &invalidDecoderError) {
+			panic(err)
+		}
+
+		return err
+	}
+
+	return nil
+}
 
 func (app *application) newTemplateData(r *http.Request) *templateData {
 	return &templateData{
--- snippetbox/cmd/web/main.go
+++ snippetbox/cmd/web/main.go
@@ -10,6 +10,7 @@ import (
 
 	"snippetbox.jackmitchellfordyce.com/internal/models"
 
+	"github.com/go-playground/form/v4"
 	_ "github.com/go-sql-driver/mysql"
 )
 
@@ -18,6 +19,7 @@ type application struct {
 	infoLog       *log.Logger
 	snippets      *models.SnippetModel
 	templateCache map[string]*template.Template
+	formDecoder   *form.Decoder
 }
 
 func main() {
@@ -40,11 +42,14 @@ func main() {
 		errorLog.Fatal(err)
 	}
 
+	formDecoder := form.NewDecoder()
+
 	app := &application{
 		errorLog:      errorLog,
 		infoLog:       infoLog,
 		snippets:      &models.SnippetModel{DB: db},
 		templateCache: templateCache,
+		formDecoder:   formDecoder,
 	}
 
 	srv := &http.Server{
