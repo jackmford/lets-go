8d3d920dee742b4d71f61d23ccee653da722a9a4
jackmford <jackmitchellfordyce@gmail.com>
2023-01-18 14:39:08 -0600

    chapter 11.7 csrf

--- snippetbox/cmd/web/helpers.go
+++ snippetbox/cmd/web/helpers.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"github.com/go-playground/form/v4"
+	"github.com/justinas/nosurf"
 )
 
 func (app *application) isAuthenticated(r *http.Request) bool {
@@ -40,6 +41,7 @@ func (app *application) newTemplateData(r *http.Request) *templateData {
 		CurrentYear:     time.Now().Year(),
 		Flash:           app.sessionManager.PopString(r.Context(), "flash"),
 		IsAuthenticated: app.isAuthenticated(r),
+		CSRFToken:       nosurf.Token(r),
 	}
 }
 func (app *application) render(w http.ResponseWriter, status int, page string, data *templateData) {
--- snippetbox/cmd/web/middleware.go
+++ snippetbox/cmd/web/middleware.go
@@ -3,7 +3,20 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/justinas/nosurf"
 )
+
+func noSurf(next http.Handler) http.Handler {
+	csrfHandler := nosurf.New(next)
+	csrfHandler.SetBaseCookie(http.Cookie{
+		HttpOnly: true,
+		Path:     "/",
+		Secure:   true,
+	})
+
+	return csrfHandler
+}
 
 func (app *application) requireAuthentication(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
