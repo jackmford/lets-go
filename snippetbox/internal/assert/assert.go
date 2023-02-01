740fbf50d9fdf4b4dcdc7fd82cbb798ec0d4aaae
jackmford <jackmitchellfordyce@gmail.com>
2023-02-01 13:40:40 -0600

    chapter 14.8 profiling test coverage (done with the book)

--- snippetbox/internal/assert/assert.go
+++ snippetbox/internal/assert/assert.go
@@ -5,6 +5,14 @@ import (
 	"testing"
 )
 
+func NilError(t *testing.T, actual error) {
+	t.Helper()
+
+	if actual != nil {
+		t.Errorf("got: %v; expected: nil", actual)
+	}
+}
+
 func Equal[T comparable](t *testing.T, actual, expected T) {
 	t.Helper()
 
